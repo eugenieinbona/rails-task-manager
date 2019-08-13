706826b16bbf5b6929cc685cfba172fa9b42bf8e
Eugenie Inbona <e.inbona@gmail.com>
2019-08-13 11:35:44 +0200

    Add Task model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'title/details'
+  get 'title/completed'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
